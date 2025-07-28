126351a3d4d3318f01aa47e553b7423ace2ae49b
Aki Kareha <aki@kareha.org>
2025-07-28 20:29:15 +0900

    clean

--- update_index.sh
+++ update_index.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cat index_head.md >index.md
+cat index_head._md >index.md
 ls screenshots|sed 's/\([^.]*\).*/* [\1](screenshots\/\1.png)/' >>index.md
 ls screenshots|sed 's/\([^.]*\).*/\n### \1\n![\1](screenshots\/\1.png)/' >>index.md
-cat index_tail.md >>index.md
+cat index_tail._md >>index.md
