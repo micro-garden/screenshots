c5857c2d690418e68fd720b91855ffb80508e95b
Aki Kareha <aki@kareha.org>
2025-08-04 19:52:15 +0900

    rename

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ find . -name '*.md' | while read -r mdfile; do
 
 	if [ ! -f "$htmlfile" ] || [ "$mdfile" -nt "$htmlfile" ]; then
 		echo "Converting: $mdfile -> $htmlfile"
-		title=$(./extract_title.pl "$mdfile")
+		title=$(./extract-title.pl "$mdfile")
 		escaped_title=$(echo $title | sed -e 's/\\/\\\\/g' -e 's/\//\\\//g' -e 's/&/\\\&/g')
 		sed "s/{{title}}/$escaped_title/" head._html >"$htmlfile"
 		cat "$mdfile" | sed -E 's/(\[[^][]+\])\((https?:\/\/[^)]+)\)/\1(\2)🔗/g' | markdown >>"$htmlfile"
